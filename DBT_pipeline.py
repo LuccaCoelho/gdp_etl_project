0edf0912baacbdfc08635065b4a82e24743a969e
Viniccius Florindo Coelho <124092017+LuccaCoelho@users.noreply.github.com>
2024-08-19 18:47:55 -0600

    Update DBT_pipeline.py

--- DBT_pipeline.py
+++ DBT_pipeline.py
@@ -5,7 +5,7 @@ import sqlite3
 import numpy as np
 from datetime import datetime
 
-url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+url = 'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
 table_attributes = ["Country", "GDP_USD_millions"]
 db_name = 'World_Economies.db'
 table_name = 'Countries_by_GDP'
